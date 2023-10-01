2d03a40a18c5b0b8314ee58b474becb6581db77d
lexa56720 <lexa.0606x@gmail.com>
2023-10-01 21:03:19 +0700

    Background services

--- TMServer/Program.cs
+++ TMServer/Program.cs
@@ -1,20 +1,26 @@
 ﻿using ApiTypes.Communication.BaseTypes;
 using TMServer.DataBase;
 using TMServer.Logger;
+using TMServer.Services;
 
 namespace TMServer
 {
     internal class Program
     {
-
         private static void Main(string[] args)
         {
-            ILogger log = new ConsoleLogger();
+            ILogger logger = new ConsoleLogger();
+
             var db = new TmdbContext();
-            ////db.Database.EnsureDeleted();
+            //db.Database.EnsureDeleted();
             db.Database.EnsureCreated();
-            var server = new Servers.TMServer(GlobalSettings.AuthPort, GlobalSettings.ApiPort,log);
+
+            var server = new Servers.TMServer(GlobalSettings.AuthPort, GlobalSettings.ApiPort,logger);
             server.Start();
+
+            var tokenCleaner = new TokenCleaner(TimeSpan.FromMinutes(5),logger);
+            tokenCleaner.Start();
+
             Console.ReadLine();
         }
     }
---
+++ TMServer/Services/Service.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMServer.Logger;
+using TMServer.ServerComponent.Basics;
+
+namespace TMServer.Services
+{
+    internal abstract class Service : Startable
+    {
+        public TimeSpan Period { get; private set; }
+        private ILogger Logger { get; }
+
+        public Service(TimeSpan period,ILogger logger)
+        {
+            Period = period;
+            Logger = logger;
+        }
+
+        public override void Start()
+        {
+            if (IsRunning)
+                return;
+            base.Start();
+            Task.Run(async () =>
+            {
+                while (IsRunning)
+                {
+                    var timer = Task.Delay(Period);
+                    await Work(Logger);
+                    await timer;
+                }
+            });
+        }
+
+        protected abstract Task Work(ILogger logger);
+    }
+}
