28d1794c36019e310d9f69e3e7f73ad011ae8b8d
lexa56720 <lexa.0606x@gmail.com>
2023-11-07 20:14:40 +0700

    Fixes

--- ApiTypes/Communication/LongPolling/LongPollingRequest.cs
+++ ApiTypes/Communication/LongPolling/LongPollingRequest.cs
@@ -4,18 +4,14 @@ namespace ApiTypes.Communication.LongPolling
 {
     public class LongPollingRequest : ISerializable<LongPollingRequest>
     {
-        public int RandomInt { get; set; } = 148;
         public static LongPollingRequest Deserialize(BinaryReader reader)
         {
-            return new LongPollingRequest()
-            {
-                RandomInt = reader.ReadInt32()
-            };
+            return new LongPollingRequest();
         }
 
         public void Serialize(BinaryWriter writer)
         {
-            writer.Write(RandomInt);
+            return;
         }
     }
 }
